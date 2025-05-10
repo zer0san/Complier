8d39720937493a8f3b51e954cb4de7e24fd2819e
zer0san <1323639983@qq.com>
2025-05-10 09:04:57 +0800

    the beginning

--- src/main/java/Parser/Expr.java
+++ src/main/java/Parser/Expr.java
@@ -9,6 +9,8 @@ import java.util.*;
 abstract class Expr {
 }
 
+
+// 数字
 class NumberExpr extends Expr {
     int value;
 
@@ -17,6 +19,31 @@ class NumberExpr extends Expr {
     }
 }
 
+// 标识符（变量）
 class VarExpr extends Expr{
+    String name;
+    VarExpr(String n){
+        this.name = n;
+    }
+}
 
+// 二元运算
+class BinaryExpr extends Expr {
+    String op;
+    Expr left, right;
+    BinaryExpr(String op, Expr left, Expr right) {
+        this.op = op;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// 赋值
+class AssignExpr extends Expr {
+    String name;
+    Expr expr;
+    AssignExpr(String name, Expr expr) {
+        this.name = name;
+        this.expr = expr;
+    }
 }
