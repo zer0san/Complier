171f220412c2c3bd1bd5cc0af5594f14a3a45e80
zer0san <1323639983@qq.com>
2025-05-10 09:07:07 +0800

    add Quadruple

---
+++ src/main/java/Parser/Quadruple.java
@@ -0,0 +1,18 @@
+package Parser;
+
+// 四元式
+public class Quadruple {
+    String op, arg1, arg2, result;
+
+    Quadruple(String op, String arg1, String arg2, String result) {
+        this.op = op;
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s %s %s %s)", op, arg1, arg2, result);
+    }
+}
