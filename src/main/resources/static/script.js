abdddbce953f45a96dd987347ce144c66215c333
zer0san <1323639983@qq.com>
2025-06-03 21:19:48 +0800

    正在添加前端网页界面

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,27 +1,28 @@
+$(document).ready(function () {
+    $('#upload').click(function (e) {
 
-$('#upload').click(function () {
-    alert("click")
-    const fileInput = $('#read_file')[0];
-    if (fileInput.files.length === 0) {
-        alert("请先选择文件！");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", fileInput.files[0]);
-
-    $.ajax({
-        url:"http://172.18.149.11:8080/parse",
-        type:"POST",
-        data:formData,
-        contentType:false,
-        processData:false,
-        success:function(data){
-            // $("#opt_area").val(data);
-            alert(data)
-        },
-        error:function (xhr, status, error) {
-            alert("failed")
+        const fileInput = $('#read_file')[0];
+        if (fileInput.files.length === 0) {
+            alert("请先选择文件！");
+            return;
         }
-    })
+
+        const formData = new FormData();
+        formData.append("file", fileInput.files[0]);
+
+        $.ajax({
+            url: "http://172.18.149.11:8080/parse",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                $("#opt_area").val(data);
+                alert(data);
+            },
+            error: function (xhr, status, error) {
+                alert("failed");
+            }
+        });
+    });
 });
