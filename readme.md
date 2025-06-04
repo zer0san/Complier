c10c27af083ae0a02d0177b8bd0571db516fa751
gy <2649263674@qq.com>
2025-06-04 10:38:16 +0800

    merge tmp into master

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # 
 1. remember to install the dependecies by maven first 
 2. start the server by running the main method in  first 
+3. the loaded file will show in the input text area first and allow the use to change it after upload the file 
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -7,19 +7,21 @@ $(document).ready(function () {
         $.ajax({
             url: "http://localhost:8080/parse",
             type: "POST",
-            data: JSON.stringify({sourceCode: $('#ipt_area').val()}),
-            contentType:"application/json",
+            data: JSON.stringify({ sourceCode: $('#ipt_area').val() }),
+            contentType: "application/json",
             processData: false,
             success: function (data) {
                 $("#opt_area").innerText = data;
-                document.getElementById("opt_area").innerText = data;
+                // document.getElementById("opt_area").innerText = data;
+                var strings = data.toString().split("\n");
+                $("#opt_area").text(strings.join("\n"));
                 console.log(data);
                 // alert(data);
             },
             error: function (xhr, status, error) {
                 $("#opt_area").style = "color: red;";
-                document.getElementById("opt_area").innerText = "解析失败，请检查输入代码是否正确！\n"+error.toString();
-                // alert("failed" + error.toString());
+                // document.getElementById("opt_area").innerText = "解析失败，请检查输入代码是否正确！\n" + error.toString();
+                alert("failed" + error.toString());
             }
         });
 
